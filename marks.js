099a25fa826437ca29760b976ebe5d906cb5fa2d
singhScriptor <dhirajkr1117@gmail.com>
2025-07-21 19:10:25 +0530

    removed some files

--- marks.js
+++
@@ -1,4 +0,0 @@
-function showResult(marks){
-    return (marks >= 40) ? "pass" : "fail";
-}
-console.log(showResult(45)); // "pass"
--- ternaryOperator.js
+++
@@ -1,5 +0,0 @@
-function largestOfTwo(a,b){
-    return (a > b) ? a : b
-
-}
-console.log(largestOfTwo(14,24)) // 24
