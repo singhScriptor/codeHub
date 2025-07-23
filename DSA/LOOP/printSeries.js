d787e56201e49867cd5c09c8cb397608f3989c42
singhScriptor <dhirajkr1117@gmail.com>
2025-07-23 17:58:14 +0530

    loop practises

---
+++ DSA/LOOP/printSeries.js
@@ -0,0 +1,25 @@
+// While loop
+
+function printSeries(n){
+    let i=1
+    while(i <= n){
+        if(i*i <= n){
+            console.log(i*i)
+        }
+        i++
+    }
+}
+printSeries(49)
+
+
+
+// for loop
+
+function ShowSeries(num){
+    for(let i=1;i<=num;i++){
+        if(i*i <= num){
+            console.log(i*i)
+        }
+    }
+}
+ShowSeries(49)
