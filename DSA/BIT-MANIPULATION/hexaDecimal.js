5736a83931b97c83eebd36e37e05fac627a0fa08
singhScriptor <dhirajkr1117@gmail.com>
2025-08-01 22:03:59 +0530

    updating

--- DSA/BIT-MANIPULATION/hexaDecimal.js
+++ DSA/BIT-MANIPULATION/hexaDecimal.js
@@ -1,6 +1,6 @@
 function hexaDecimals(num){
-    if (num === 0) return '0'
-    if(num < 0) num = num >>> 0
+    if(num === 0) return '0'
+    if(num < 0) num=num >>> 0
     let m='0123456789abcdef'
     let ans=''
     for(let i=1;i<=8;i++){
