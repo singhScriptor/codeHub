18578e904950f8e374b939828e929d445f4b250d
singhScriptor <dhirajkr1117@gmail.com>
2025-08-18 07:22:27 +0530

    core of set and binary search

---
+++ DSA/OOPS/peakElement.js
@@ -0,0 +1,16 @@
+function peakELement(nums){
+    let left=0,right=nums.length-1
+    while(left<right){
+        let mid=Math.floor((left+right)/2)
+        if(nums[mid]<nums[mid+1]){
+            left=mid+1
+        }
+        else{
+            right=mid-1
+        }
+    }
+    return left
+
+}
+let nums=[1,2,3,1]
+console.log(peakELement(nums))
