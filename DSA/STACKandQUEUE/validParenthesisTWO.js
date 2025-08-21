2d5452b1d1769a67b916ac6cfe8365323fe7165d
singhScriptor <dhirajkr1117@gmail.com>
2025-08-21 11:13:08 +0530

    working on dsa with stack and queue

---
+++ DSA/STACKandQUEUE/validParenthesisTWO.js
@@ -0,0 +1,18 @@
+function validParethesis(s){
+    let dict={']':'[',')':'(','}':'{'}
+    let stack=[]
+    for(let i of s){
+        if(i in dict){
+            if(stack.length === 0 || stack.pop() !== dict[i]){
+                return false
+            }
+        }
+        else{
+            stack.push(i)
+        }
+    }
+    return stack.length===0
+    
+}
+let s='{}'
+console.log(validParethesis(s))
