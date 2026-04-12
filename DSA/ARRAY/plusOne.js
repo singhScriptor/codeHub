20e2bfd3d54e9912294ab6893363ba062e697d0a
starlord <dhirajkr1117@gmail.com>
2026-04-12 19:59:31 +0530

    dsa question plus one

--- DSA/ARRAY/plusOne.js
+++ DSA/ARRAY/plusOne.js
@@ -1,1 +1,13 @@
-function plusOne
+function plusOne(digits){
+    for(let i= digits.length-1;i>=0;i--){
+        if(digits[i] < 9){
+            digits[i]++
+            return digits
+        }
+        digits[i]=0
+    }
+    digits.unshift(1)
+    return digits
+}
+let digits=[4,9,9]
+console.log(plusOne(digits))
